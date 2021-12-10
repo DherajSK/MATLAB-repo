41ab259c78299443fb3e67b82a8b2638e6a38cb0
DherajSK <66167770+DherajSK@users.noreply.github.com>
2021-12-10 12:37:17 +0530

    LAB5

---
+++ Chebyshevstop.m
@@ -0,0 +1,12 @@
+clc;
+clear all;
+close all;
+rp=input('Enter the passband attenuation:');
+rs=input('enter the stopband attenduation:');
+wp=input('Enter the passband frequency:');
+ws=input('Enter the stopband frequency:');
+
+
+[N,wn]=cheb1ord(wp/pi, ws/pi,rp, rs);
+[b,a]=cheby1(N,rp,wn,'stop');
+freqz(b,a);
---
+++ Digitalbutterworthlow.m
@@ -0,0 +1,12 @@
+clc;
+clear all;
+close all;
+rp=input('Enter the passband attenuation:');;
+rs=input('enter the stopband attenduation:');
+wp=input('Enter the passband frequency:');
+ws=input('Enter the stopband frequency:');
+
+%low pass
+[N,wn]=buttord(wp/pi, ws/pi,rp, rs);
+[b,a]=butter(N,wn);
+freqz(b,a);
